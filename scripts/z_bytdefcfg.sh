c2a39f84077cb9f961fd93e8269110d41265bc37
zefie <zefie@zefie.net>
2016-05-11 12:42:00 -0400

    build 64-bit kernel by default for nextbook and baytrail

--- scripts/z_bytdefcfg.sh
+++ scripts/z_bytdefcfg.sh
@@ -2,3 +2,6 @@
 cp arch/x86/configs/nextbook_defconfig arch/x86/configs/baytrail_defconfig
 sed -i 's|CONFIG_ACPI_CUSTOM_DSDT_FILE="/home/zefie/dsdt.hex"|# CONFIG_ACPI_CUSTOM_DSDT_FILE is not set|' arch/x86/configs/baytrail_defconfig
 sed -i 's|CONFIG_ACPI_CUSTOM_DSDT=y|# CONFIG_ACPI_CUSTOM_DSDT is not set|' arch/x86/configs/baytrail_defconfig
+sed -i 's|CONFIG_LOGO=y|# CONFIG_LOGO is not set|' arch/x86/configs/baytrail_defconfig
+sed -i 's|CONFIG_LOGO_NEXTBOOK_CLUT224=y|# CONFIG_LOGO_NEXTBOOK_CLUT224 is not set|' arch/x86/configs/baytrail_defconfig
+
